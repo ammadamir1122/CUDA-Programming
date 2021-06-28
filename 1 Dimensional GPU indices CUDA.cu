7571319d38174d9cefa66ce9c57de900efb5a107
Ammad Amir <62566224+ammadamir1122@users.noreply.github.com>
2021-06-28 17:05:33 +0500

    Update 1 Dimensional GPU indices CUDA.cu

--- 1 Dimensional GPU indices CUDA.cu
+++ 1 Dimensional GPU indices CUDA.cu
@@ -2,20 +2,20 @@
 */
 #include <stdio.h>
 __global__ void myHelloOnGPU(int *array) {
-array[blockIdx.x] = blockIdx.x
+    array[blockIdx.x] = blockIdx.x
 }
 int main() {
-int N = 16;
-int *cpuArray = (int*)malloc(sizeof(int)*N);
-int *gpuArray;
-cudaMalloc((void **)&gpuArray, sizeof(int)*N);
-myHelloOnGPU<<<N, 1>>>(gpuArray);
-cudaMemcpy(cpuArray, gpuArray, sizeof(int)*N,
-cudaMemcpyDeviceToHost);
-int i;
-for (i = 0; i < N; i++) {
-printf("%d ", cpuArray[i]);
-}
+    int N = 16;
+    int *cpuArray = (int*)malloc(sizeof(int)*N);
+    int *gpuArray;
+    cudaMalloc((void **)&gpuArray, sizeof(int)*N);
+    myHelloOnGPU<<<N, 1>>>(gpuArray);
+    cudaMemcpy(cpuArray, gpuArray, sizeof(int)*N,
+    cudaMemcpyDeviceToHost);
+    int i;
+    for (i = 0; i < N; i++) {
+        printf("%d ", cpuArray[i]);
+    }
 printf("\n");
 return 0;
 }
